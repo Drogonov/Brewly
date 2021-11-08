5bfdb76f132f4345e617b2f304db6d022b91ae3c
Anton Drogonov <vlezko.anton@yandex.ru>
2021-11-08 13:23:41 +0300

    Add some design to BrewItemView

--- Brewly/Modules/Brew/BrewItem/BrewItemView.swift
+++ Brewly/Modules/Brew/BrewItem/BrewItemView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct BrewItemView: View {
+    @State private var aromaValue: Double = 0
+    @State private var comment: String = ""
+    @State var hidePlaceholder: Bool = false
+    
     var body: some View {
-        Text("Hello, Sucker!")
+        List {
+            sectionBuilder(title: "Aroma")
+            sectionBuilder(title: "Acidity")
+            sectionBuilder(title: "Sweetness")
+        }
     }
+    
+    func sectionBuilder(title: String) -> some View {
+        Section {
+            sliderView(title: "Intensity", value: "Strong")
+            sliderView(title: "Quality", value: "Weak")
+            commentView()
+        } header: {
+            Text(title)
+        }
+    }
+    
+    func commentView() -> some View {
+        ZStack(alignment: .leading) {
+            ZStack {
+                TextEditor(text: $comment)
+                    .onTapGesture {
+                        hidePlaceholder = true
+                    }
+                Text(comment).opacity(0).padding(.all, 8)
+            }
+            Text("Comment")
+                .opacity(hidePlaceholder ? 0 : 1)
+                .foregroundColor(.gray)
+                .onChange(of: comment) { newComment in
+                    if newComment != "" {
+                        hidePlaceholder = true
+                    } else {
+                        hidePlaceholder = false
+                    }
+                }
+        }
+    }
+    
+    func sliderView(title: String, value: String) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .font(.body)
+                    .foregroundColor(.primary)
+                
+                Spacer()
+
+                Text(value)
+                    .font(.body)
+                    .foregroundColor(.primary)
+            }
+            
+            Slider(value: $aromaValue,
+                   in: 0...4)
+            
+        }
+    }
+    
 }
 
 struct BrewItemView_Previews: PreviewProvider {
