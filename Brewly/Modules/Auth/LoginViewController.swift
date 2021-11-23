21e7b5add233d8b84e38e939f734fbdcf1a8811b
Anton Vlezko <vlezko.anton@yandex.ru>
2021-11-23 10:09:45 +0300

    add pods and Google sign in

--- Brewly/Modules/Auth/LoginViewController.swift
+++ Brewly/Modules/Auth/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftUI
+import GoogleSignIn
 
 protocol LoginViewProtocol: AnyObject {
     func setLoginView(with _model: AuthViewModel)
@@ -20,6 +21,12 @@ class LoginViewController: BaseViewController {
     
     // MARK: - Lifecycle
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -61,3 +68,42 @@ extension LoginViewController: LoginViewProtocol {
         configureView(with: _model)
     }
 }
+
+extension LoginViewController: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("Failed to sign in with error:", error)
+            return
+        }
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { (result, error) in
+            if let error = error {
+                print("Failed to sign in and retrieve data with error:", error)
+                return
+            }
+            
+            guard let uid = result?.user.uid else { return }
+            guard let email = result?.user.email else { return }
+            guard let username = result?.user.displayName else { return }
+            guard let photoURL = result?.user.photoURL else { return }
+            
+            let photoURLString = photoURL.absoluteString
+            
+            let values = ["email": email,
+                          "fullname": username,
+                          "profileImageUrl": photoURLString] as [String : Any]
+            
+            Service.shared.updateUserValues(uid: uid, values: values) { (err, ref) in
+                if let error = error {
+                    print("Failed to upload data to server GIDSignIn with error:", error)
+                    return
+                }
+                self.delegate?.userLoginVC(self)
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
+
